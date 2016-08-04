5654b305081bc195027ce00302b38af1fb0e249b
Oleg Broytman <phd@phdru.name>
2016-08-04 20:00:42 +0300

    Refactor 'else if' to 'elif'

--- mysql2sql/process_tokens.py
+++ mysql2sql/process_tokens.py
@@ -8,12 +8,11 @@ def requote_names(token_list):
     for token in token_list:
         if isinstance(token, TokenList):
             requote_names(token)
-        else:
-            if token.ttype is Name:
-                value = token.value
-                if (value[0] == "`") and (value[-1] == "`"):
-                    value = value[1:-1]
-                if value.islower():
-                    token.normalized = token.value = value
-                else:
-                    token.normalized = token.value = '"%s"' % value
+        elif token.ttype is Name:
+            value = token.value
+            if (value[0] == "`") and (value[-1] == "`"):
+                value = value[1:-1]
+            if value.islower():
+                token.normalized = token.value = value
+            else:
+                token.normalized = token.value = '"%s"' % value
