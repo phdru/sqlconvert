05b5fb4c6b4e167ffefbd17e9530d87db027d942
Oleg Broytman <phd@phdru.name>
2016-09-05 22:18:29 +0300

    Add a semicolon and a newline after directive

--- demo/demo.sql
+++ demo/demo.sql
@@ -1,6 +1,7 @@
 SELECT * FROM `mytable`; -- line-comment"
 INSERT into /* inline comment */ mytable VALUES (1, 'тест');
-/*! directive*/ INSERT INTO `MyTable` (`Id`, `Name`)
+/*! directive*/;
+INSERT INTO `MyTable` (`Id`, `Name`)
 VALUES (1, 'one');
 
 -- The end
