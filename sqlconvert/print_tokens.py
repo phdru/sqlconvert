6b991d668a6f384e272fb19f09187a2d9407f1b3
Oleg Broytman <phd@phdru.name>
2016-09-07 22:48:38 +0300

    Fix: flush buffer and outfile

--- sqlconvert/print_tokens.py
+++ sqlconvert/print_tokens.py
@@ -4,12 +4,15 @@ import sys
 
 def print_tokens(token_list, outfile=sys.stdout, encoding=None):
     if encoding:
-        outfile = getattr(outfile, 'buffer', outfile)
+        buffer = getattr(outfile, 'buffer', outfile)
     for token in token_list.flatten():
         normalized = token.normalized
         if encoding:
             normalized = normalized.encode(encoding)
-        outfile.write(normalized)
+        buffer.write(normalized)
+    if buffer is not outfile:
+        buffer.flush()
+    outfile.flush()
 
 
 def tlist2str(token_list):
