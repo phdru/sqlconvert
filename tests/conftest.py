ff5a3c5c7a9ae00017e1427bdfb15bc7cb3b89e1
Oleg Broytman <phd@phdru.name>
2016-09-25 04:02:12 +0300

    Exclude tests/conftest.py from flake8 tests with directive

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
-# noqa: imported for pytest
+# flake8: noqa
+# Skip testing the entire module: names here are imported for pytest
 
 from sqlobject.conftest import \
     pytest_addoption, pytest_configure, setup_tests
