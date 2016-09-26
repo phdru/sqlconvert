2b8ee2f51e148fd5ffb1ff41143c50c1fa8e815d
Oleg Broytman <phd@phdru.name>
2016-09-27 00:54:23 +0300

    Add a test for print_tokens()

---
+++ tests/test_print_tokens.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    try:
+        from StringIO import StringIO
+    except ImportError:
+        from io import StringIO
+
+from sqlparse import parse
+from sqlconvert.print_tokens import print_tokens
+
+
+def test_print_tokens():
+    sio = StringIO()
+    parsed = parse("select * from T")[0]
+    print_tokens(parsed, outfile=sio)
+    assert sio.getvalue() == u"SELECT * FROM T"
