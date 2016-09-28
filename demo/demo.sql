7bc5c10272d7c58e141aea6bc781fe1173767d5f
Oleg Broytman <phd@phdru.name>
2016-09-28 19:06:58 +0300

    Extend demo.sql: add extended INSERTs with and w/o columns list

--- demo/demo.sql
+++ demo/demo.sql
@@ -1,7 +1,14 @@
 SELECT * FROM `mytable`; -- line-comment"
+
 INSERT into /* inline comment */ mytable VALUES (1, 'тест');
+
 /*! directive*/;
+
 INSERT INTO `MyTable` (`Id`, `Name`)
 VALUES (1, 'one');
+
+insert into mytable values (1, 'one'), (1, 'one');
+
+insert into mytable (id, name) values (1, 'one'), (1, 'one');
 
 -- The end
