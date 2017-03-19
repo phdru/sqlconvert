d44a7df77b67d6a0b6522fcd5ba9e19ea71c3241
Oleg Broytman <phd@phdru.name>
2017-03-19 19:17:00 +0300

    Update extended INSERTs in demo.sql

--- demo/demo.sql
+++ demo/demo.sql
@@ -7,8 +7,8 @@ INSERT into /* inline comment */ mytable VALUES (1, 'тест');
 INSERT INTO `MyTable` (`Id`, `Name`)
 VALUES (1, 'one');
 
-insert into mytable values (1, 'one'), (1, 'one');
+insert into mytable values (1, 'one'), (2, 'two');
 
-insert into mytable (id, name) values (1, 'one'), (1, 'one');
+insert into mytable (id, name) values (1, 'one'), (2, 'two');
 
 -- The end
