51de618d107a67b76278d5c3a003abbf22d974bf
Oleg Broytman <phd@phdru.name>
2019-02-18 21:32:33 +0300

    Style(setup): Fix flake8 F821 undefined name `execfile` under Python 3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ versionpath = join(abspath(dirname(__file__)), 'sqlconvert', '__version__.py')
 sqlconvert_version = {}
 
 if sys.version_info[:2] == (2, 7):
-    execfile(versionpath, sqlconvert_version)
+    execfile(versionpath, sqlconvert_version)  # noqa: F821 'execfile' Py3
 
 elif sys.version_info >= (3, 4):
     exec(open(versionpath, 'rU').read(), sqlconvert_version)
