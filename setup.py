3f940336d4886d6a72275b6a6d1fb78db0acf562
Oleg Broytman <phd@phdru.name>
2022-09-19 20:58:15 +0300

    Build(setup.py): Python 3.9, 3.10

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     packages=['sqlconvert'],
     scripts=['scripts/mysql2sql'],
